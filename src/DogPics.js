26360e1e83dbe0a0a64864e3b55033990f4ef128
maureen <maureen.chelangat@student.moringaschool.com>
2023-11-01 11:38:57 +0300

    completed assignment

--- src/DogPics.js
+++ src/DogPics.js
@@ -5,25 +5,22 @@ import React, { useState, useEffect } from "react";
 function DogPics() {
   const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    console.log("useEffect");
-    fetch("https://dog.ceo/api/breeds/image/random/3")
-      .then((r) => r.json())
-      .then((data) => {
-        console.log("setState");
-        setImages(data.message);
-      });
-  });
+  useEffect(()=>{
+    fetch ("https://dog.ceo/api/breeds/image/random/3")
+    .then((res)=>res.json())
+    .then((data)=>{
+      setImages(data.message);
+    });
+  }, []);
 
-  console.log("render");
-
-  return (
+  return(
     <div>
-      {images.map((image) => (
-        <img src={image} key={image} />
+      {images.map((image)=>(
+        <img src={image} key={image}/>
       ))}
     </div>
   );
+  
 }
 
 export default DogPics;
